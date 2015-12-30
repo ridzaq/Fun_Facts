e1c5d05c10b55f9ba6afd551b6c2b6ea2fbe6de3
Rizal <ridzaq@yahoo.com>
2015-12-30 23:09:29 +0800

    Final commit in course. Added log to logcat.

--- app/src/main/java/com/example/ahmed/funfacts/MainActivity.java
+++ app/src/main/java/com/example/ahmed/funfacts/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.ahmed.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public static  final String TAG = MainActivity.class.getSimpleName();
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
     private TextView mFactTextView;
@@ -37,5 +40,8 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mShowFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(MainActivity.this, "Yay! Our Activity was created", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
